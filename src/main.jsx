b35725649d163e000a1bb1a79f615b54eecdbcfd
thiagogcb <thiago202017@gmail.com>
2026-05-19 14:03:41 -0300

    Fix import statements in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactDOimport React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from M from 'react-dom/client';
 import App from './App';
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
